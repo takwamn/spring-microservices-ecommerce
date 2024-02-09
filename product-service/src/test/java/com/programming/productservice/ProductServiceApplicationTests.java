d270a1732cec0367c5fdad389eb35d4bda7e5ec7
takwamn <takwa.manai@sesame.com.tn>
2024-02-09 09:37:54 +0100

    Initial Commit

--- product-service/src/test/java/com/programming/productservice/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/programming/productservice/ProductServiceApplicationTests.java
@@ -2,12 +2,24 @@ package com.programming.productservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+
+    static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
+    @Test
+    void contextLoads() {
+    }
 
 }
